95e0418563688897c965d391fcbd0b897775085c
Outwater <kocon135@gmail.com>
2021-03-24 00:39:55 +0900

    Add conditions for get question

--- controllers/index.js
+++ controllers/index.js
@@ -2,50 +2,73 @@ const models = require('../models');
 const { isAuthorized } = require('./tokenFunctions');
 const { getUserIdByToken } = require('../common');
 const { answer, question } = models;
+const Sequelize = require('sequelize');
+const Op = Sequelize.Op;
 
 const moment = require('moment');
-require('moment-timezone');
-moment.tz.setDefault('Asia/Seoul');
-moment().format();
 moment().format('YYYY-MM-DD');
 module.exports = {
   // 비어있는 질문-대답 생성
   intro: async (req, res) => {
-    const accessTokenData = isAuthorized(req);
-    if (!accessTokenData) {
-      return res.json({ data: null, message: 'invalid access token' });
-    }
-
+    //token검사
+    const accessTokenData = isAuthorized(req, res);
     const requestUserId = await getUserIdByToken(accessTokenData);
-    // 생성 될 질문번호
-    const questionIndex =
-      (await answer.max('questionId', {
-        where: { userId: requestUserId },
-      })) + 1 || 1;
 
-    // 빈 질문지 생성
-    const koreaTime = await moment.tz(moment().toDate(), 'Asia/Seoul').format();
-    await answer.create({
-      content: '',
-      questionAt: moment().toDate(),
-      userId: requestUserId,
-      questionId: questionIndex,
+    // 오늘 질문 받은 여부(개수) 체크
+    const isAlreadyGetQuestion = await answer.findAll({
+      where: {
+        userId: requestUserId,
+        questionAt: { [Op.gt]: new Date().toLocaleDateString() },
+      },
     });
 
-    // 쿼리로 원하는 속성 찾아 response
-    await answer
-      .findOne({
-        include: [{ model: question, where: { id: questionIndex } }],
-        where: { userId: requestUserId },
-        attributes: ['id', 'questionAt'],
-      })
-      .then(data => {
-        res.status(200).json({
-          questionId: data.dataValues.question.dataValues.id,
-          questionContent: data.dataValues.question.dataValues.content,
-          answerId: data.dataValues.id,
-          questionAt: data.dataValues.questionAt,
+    // 이미 질문 받았다면
+    if (isAlreadyGetQuestion.length > 0) {
+      await answer
+        .findOne({
+          include: [{ model: question, where: { id: questionIndex - 1 } }],
+          where: { userId: requestUserId },
+          attributes: ['id', 'questionAt'],
+        })
+        .then(data => {
+          res.status(200).json({
+            questionId: data.dataValues.question.dataValues.id,
+            questionContent: data.dataValues.question.dataValues.content,
+            answerId: data.dataValues.id,
+            questionAt: data.dataValues.questionAt,
+          });
         });
+    } else {
+      // 오늘 질문 받은 적 없다면
+
+      // 질문(빈answer) 생성
+      const questionIndex =
+        (await answer.max('questionId', {
+          where: { userId: requestUserId },
+        })) + 1 || 1;
+
+      await answer.create({
+        content: '',
+        questionAt: moment().toDate(),
+        userId: requestUserId,
+        questionId: questionIndex,
       });
+
+      // 쿼리로 원하는 속성 찾아 response
+      await answer
+        .findOne({
+          include: [{ model: question, where: { id: questionIndex } }],
+          where: { userId: requestUserId },
+          attributes: ['id', 'questionAt'],
+        })
+        .then(data => {
+          res.status(200).json({
+            questionId: data.dataValues.question.dataValues.id,
+            questionContent: data.dataValues.question.dataValues.content,
+            answerId: data.dataValues.id,
+            questionAt: data.dataValues.questionAt,
+          });
+        });
+    }
   },
 };
